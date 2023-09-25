a69e32fe4ba8a073b1d16b032b8aff0bc07d232f
juthamas saikaew <juthamas.s@piesoft.net>
2023-09-25 18:06:26 +0700

    update controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using LastguyShop.Models;
+﻿using LastguyShop.Data.Context;
+using LastguyShop.Data.Entities;
+using LastguyShop.Models;
+using LastguyShop.Models.Product;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +10,12 @@ namespace LastguyShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly LastguyShopContext _lastguyShopContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, LastguyShopContext lastguyShopContext)
         {
             _logger = logger;
+            _lastguyShopContext = lastguyShopContext;
         }
 
         public IActionResult Index()
@@ -20,6 +25,27 @@ namespace LastguyShop.Controllers
 
         public IActionResult ListProduct()
         {
+            var objectProduct = _lastguyShopContext.Products.Where(i => i.IsDelete == 0).ToList();
+            var objectPrice = _lastguyShopContext.HistoryPrices.Where(i => i.IsDelete == 0).OrderByDescending(o => o.CreatedDate).ToList();
+            var objectSupplier = _lastguyShopContext.Suppliers.Where(i => i.IsDelete == 0).OrderByDescending(o => o.CreatedDate).ToList();
+            var objectProductList = new List<ListProduct>();
+            if (objectProduct != null)
+            {
+                foreach (var item in objectProduct)
+                {
+                    objectProductList.Add(new ListProduct
+                    {
+                        productId = item.ProductId,
+                        productName = item.Name,
+                        price = 0,
+                        totalAmount = item.TotalAmount.HasValue ? item.TotalAmount.Value : 0,
+                        unit = item.Unit,
+                        description = item.Description,
+                        note = item.Note,
+                        supplierName = ""
+                    });
+                }
+            }
             return View();
         }
 
