dcd7b2d02092bb11abcd297c0386afaf6150f2e3
juthamas saikaew <juthamas.s@piesoft.net>
2023-10-17 14:20:57 +0700

    common controller

---
+++ LastguyShop/Controllers/CommonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LastguyShop.Controllers
+{
+    public class CommonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationMember()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+
+        public IActionResult FileManagement()
+        {
+            return View();
+        }
+    }
+}
