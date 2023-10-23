9ae06880c1d89cd5c9d7340bbd63b8e0d33a5b86
Juthamas Saikaew <pyjamassamajyp@gmail.com>
2023-10-23 22:13:42 +0700

    test barcode

--- LastguyShop/Controllers/CommonController.cs
+++ LastguyShop/Controllers/CommonController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
 namespace LastguyShop.Controllers
 {
     public class CommonController : Controller
@@ -26,6 +27,35 @@ namespace LastguyShop.Controllers
 
         public IActionResult FileManagement()
         {
+            return View();
+        }
+
+        public IActionResult BarCode()
+        {
+            return View();
+        }
+
+        public IActionResult ReadBarCode()
+        {
+            //Barcode barCode = new Barcode();
+            //var img = (Image)barCode.Encode(BarcodeStandard.Type.Code39,"256987",250,100);
+            //var data = ConvertImageToBytes(img);
+            return View();
+        }
+
+        //public byte[] ConvertImageToBytes()
+        //{
+        //    //using (MemoryStream ms = new MemoryStream())
+        //    //{
+        //    //    img.Save(ms,System.Drawing.Imaging.ImageFormat.Png);
+        //    //    return ms.ToArray();
+        //    //}
+        //}
+
+        public IActionResult WriteBarCode(string code)
+        {
+            
+
             return View();
         }
     }
--- LastguyShop/Controllers/HomeController.cs
+++ LastguyShop/Controllers/HomeController.cs
@@ -671,10 +671,10 @@ namespace LastguyShop.Controllers
 
         #endregion
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
